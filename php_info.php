446f4a002dd48d9e2c61e07cb3ef9dba27adf8bc
Josh Knight <josh@seven-seventeen.com>
2013-04-06 21:29:51 -0400

    Testing Soap Client

--- php_info.php
+++ php_info.php
@@ -1,4 +1,15 @@
 <?php 
+# SOAP_Hello.php
+# Copyright (c) 2009 by Dr. Herong Yang, herongyang.com
+# All rights reserved
+   $client = new SoapClient("http://www.herongyang.com/Service/Hello_WSDL_11_SOAP.wsdl");
+   echo $client->Hello("Hello from client.");
+?>
+
+
+
+
+<?php 
 
 phpinfo();
 
